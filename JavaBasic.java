dc89cba0caca42c8d88965d8c2ef42813fa39138
Subhas kushwaha <167758912+subhaskushwaha@users.noreply.github.com>
2025-06-07 11:38:01 +0530

    Add files via upload
    code push

---
+++ JavaBasic.java
@@ -0,0 +1,71 @@
+// import java.util.*;
+
+// public class JavaBasic {
+
+//     public static void main(String args[]){
+//          byte b = 8 ;
+//          System.out.println(b);
+//          char ch = 'a';
+//          System.out.println(ch);
+//          boolean var = false;
+//          float price = 10; 
+//          int number = 23;
+//          //long 
+//          //double 
+//          short n = 240;
+//     }
+// }
+// //boilerplate code 
+
+import java.util.*;
+
+public class JavaBasic {
+
+    public static void main(String args[]){
+    //    Scanner sc = new Scanner(System.in);
+    //    String name = sc.next();
+    //    System.out.println(name);
+    //       String name = sc.nextLine();
+    //       System.out.println(name);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int product = a * b ;
+        // System.out.println("mul of two number = "  +product);
+
+        // int number = sc.nextInt();
+        // System.out.println(number);
+
+        // float price = sc.nextFloat();
+        // System.out.println(price);
+
+        // boolean name = sc.nextBoolean();
+        // System.out.println(name);
+
+        // short name = sc.nextShort();
+        // System.out.println(name);
+
+        // Long name = sc.nextLong();
+        // System.out.println(name);
+
+        // int a = 25;
+        // long b = a;
+        // System.out.println(b);
+
+        // float a = 25.9999f;
+        // int b = (int) a;
+        // System.out.println(a);
+
+        // char a = 'a';
+        // char b = 'b';
+        // System.out.println((int)(b)); 
+        // System.out.println((int)(a));
+        // System.out.println(b-a);
+
+        int a = 10;
+        float b = 20.25f;
+        long c = 25; 
+        double d = 30;
+        double ans = a + b + c + d;
+        System.out.println(ans);
+    }
+}
